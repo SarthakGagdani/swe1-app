ee9ab4e0639edcb4dfeb24270b9aa942addf6b44
Sarthak Gagdani <sg6367@nyu.edu>
2022-10-18 14:49:24 -0400

    changed settings.py

--- swe1/settings.py
+++ swe1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['poll-app.eba-tqkfvyrx.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
